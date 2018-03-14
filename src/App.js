6a694d5caee52e5c06f816d05058c2553eb6ee36
Andrew Van Slaars <avanslaars@gmail.com>
2018-03-13 20:53:54 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to React</h1>
+        <h2>Now with more parcel</h2>
+      </div>
+    )
+  }
+}
